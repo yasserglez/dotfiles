c6f21c1c99b69014cf540e1c640e172cb268c562
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-10-17 19:05:24 -0400

    Honor LS_COLORS in zsh completions

--- fabfile.py
+++ fabfile.py
@@ -8,25 +8,25 @@ def oh_my_zsh():
     local('chsh -s /bin/zsh')
     local('ln -sf "$PWD/zshrc" ~/.zshrc')
 
-def solarized(scheme='dark'):
+def solarized(scheme='light'):
     solarized_dircolors(scheme)
     solarized_gnome_terminal(scheme)
     solarized_gedit(scheme)
 
-def solarized_gnome_terminal(scheme='dark'):
+def solarized_gnome_terminal(scheme='light'):
     with lcd('gnome-terminal-colors-solarized'):
         local('sudo apt-get install -y dconf-cli')
         local('./install.sh -s {scheme} -p Default'.format(scheme=scheme))
         
-def solarized_dircolors(scheme='dark'):
+def solarized_dircolors(scheme='light'):
     with lcd('dircolors-solarized'):
         local('rm -f ~/.dircolors')
         local('ln -s "$PWD/dircolors.ansi-{scheme}" ~/.dircolors'.format(scheme=scheme))
         local('eval `dircolors ~/.dircolors`')
         
-def solarized_gedit(scheme='dark'):
+def solarized_gedit(scheme='light'):
     with lcd('solarized-gedit'):
         local('mkdir -p ~/.local/share/gedit/styles')
-        local('ln -sf "$PWD/solarized-dark.xml" ~/.local/share/gedit/styles/')
+        local('ln -sf "$PWD/solarized-light.xml" ~/.local/share/gedit/styles/')
         local('ln -sf "$PWD/solarized-dark.xml" ~/.local/share/gedit/styles/')
 
--- zshrc
+++ zshrc
@@ -1,20 +1,28 @@
-
-% oh-my-zsh configuration
+# oh-my-zsh configuration
 
 export ZSH=$HOME/.oh-my-zsh
+
+SOLARIZED_THEME="light"
 ZSH_THEME="blinks"
+
 plugins=(command-not-found git python pip fabric)
 source $ZSH/oh-my-zsh.sh
 
 # Customize the blinks theme
 
-SOLARIZED_THEME="light"
 PROMPT='%{%f%k%b%}
 %{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
 %{%K{${bkg}}%} %#%{%f%k%b%} '
 RPROMPT=''
 
-# Setting the PATH
+# Same colors in ls and zsh completions
 
-# 'pip --user' installed packages
-export PATH=/home/ygf/.local/bin:$PATH
+eval `dircolors ~/.dircolors`
+alias ls='ls --color=auto'
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
+autoload -Uz compinit
+compinit
+
+# $PATH settings
+
+export PATH="~/.local/bin:$PATH"
