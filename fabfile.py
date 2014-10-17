3086338610342bcfff5c88d6a929cee4a6002a9b
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-10-17 15:43:42 -0400

    Set dark as the default Solarized scheme

--- fabfile.py
+++ fabfile.py
@@ -8,25 +8,25 @@ def oh_my_zsh():
     local('chsh -s /bin/zsh')
     local('ln -sf "$PWD/zshrc" ~/.zshrc')
 
-def solarized(scheme='light'):
+def solarized(scheme='dark'):
     solarized_dircolors(scheme)
     solarized_gnome_terminal(scheme)
     solarized_gedit(scheme)
 
-def solarized_gnome_terminal(scheme='light'):
+def solarized_gnome_terminal(scheme='dark'):
     with lcd('gnome-terminal-colors-solarized'):
         local('sudo apt-get install -y dconf-cli')
         local('./install.sh -s {scheme} -p Default'.format(scheme=scheme))
         
-def solarized_dircolors(scheme='light'):
+def solarized_dircolors(scheme='dark'):
     with lcd('dircolors-solarized'):
         local('rm -f ~/.dircolors')
         local('ln -s "$PWD/dircolors.ansi-{scheme}" ~/.dircolors'.format(scheme=scheme))
         local('eval `dircolors ~/.dircolors`')
         
-def solarized_gedit(scheme='light'):
+def solarized_gedit(scheme='dark'):
     with lcd('solarized-gedit'):
         local('mkdir -p ~/.local/share/gedit/styles')
-        local('ln -sf "$PWD/solarized-light.xml" ~/.local/share/gedit/styles/')
+        local('ln -sf "$PWD/solarized-dark.xml" ~/.local/share/gedit/styles/')
         local('ln -sf "$PWD/solarized-dark.xml" ~/.local/share/gedit/styles/')
 
