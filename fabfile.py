b3311d8ad6c0bc1af01c243659f9ca425e898d6e
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-10-17 11:33:09 -0400

    Fabric task to configure the Solarized dircolors theme

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,16 @@ from fabric.api import *
 
 
 def solarized(scheme='light'):
+    solarized_dircolors(scheme)
     solarized_gnome_terminal(scheme)
-
 
 def solarized_gnome_terminal(scheme='light'):
     with lcd('gnome-terminal-colors-solarized'):
         local('sudo apt-get install -y dconf-cli')
         local('./install.sh -s {scheme} -p Default'.format(scheme=scheme))
+        
+def solarized_dircolors(scheme='light'):
+    with lcd('dircolors-solarized'):
+        local('rm -f ~/.dircolors')
+        local('ln -s "$PWD/dircolors.ansi-{scheme}" ~/.dircolors'.format(scheme=scheme))
+        local('eval `dircolors ~/.dircolors`')
