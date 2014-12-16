d5105ce80ef3d746b32774c34f5811e5185c91e4
Yasser Gonzalez Fernandez <contact@yglezfdez.com>
2014-12-15 23:28:42 -0500

    Stop Emacs from asking if a link should be followed at startup

--- emacs/init.el
+++ emacs/init.el
@@ -2,5 +2,6 @@
 
 ;; Everything is contained in one Org-babel file that is loaded here.
 (require 'org)
+(setq vc-follow-symlinks nil)
 (defvar ygf/config-file "~/.emacs.d/config.org")
 (org-babel-load-file ygf/config-file)
