26b423e078d6914ea6308dc879cd197bc1d9a596
Yasser Gonzalez Fernandez <contact@yglezfdez.com>
2014-12-25 01:19:26 -0500

    Add a Fabric task to install some Ubuntu packages

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ from fabric.contrib.console import confirm
 
 
 def install_all(force=False):
+    install_packages()
     install_zsh(force)
     install_git(force)
     install_emacs(force)
@@ -14,6 +15,29 @@ def install_all(force=False):
     install_solarized_gedit(force)
     install_r(force)
 
+
+def install_packages():
+    packages = """
+    banshee
+    easytag
+    encfs
+    gimp
+    gnome-encfs-manager
+    gnucash
+    htop
+    inkscape
+    iotop
+    jabref
+    kile
+    lyx
+    meld
+    texlive-lang-english
+    texlive-lang-spanish
+    vlc    
+    """.strip().split()
+    _apt_get_install(*packages)
+
+
 def install_r(force=False):
     _apt_get_install('r-base', 'r-base-dev')
     if force or _can_overwrite('~/.Rprofile'):
@@ -21,6 +45,7 @@ def install_r(force=False):
         local('mkdir -p ~/.local/lib/R/library')
         with lcd('R'):
             local('ln -s $PWD/Rprofile ~/.Rprofile')
+
 
 def install_emacs(force=False):
     _apt_get_install('emacs24')
@@ -30,6 +55,7 @@ def install_emacs(force=False):
         with lcd('emacs'):
             local('ln -s $PWD/init.el ~/.emacs.d/init.el')
             local('ln -s $PWD/config.org ~/.emacs.d/config.org')
+
 
 def install_vim(force=False):
     _apt_get_install('git', 'vim-nox')
@@ -45,6 +71,7 @@ def install_vim(force=False):
         _git_pull_or_clone(git_repo, git_repo_dir)
         with lcd('vim'):
             local('ln -sf $PWD/vundle ~/.vim/vundle')
+
 
 def install_git(force=False):
     _apt_get_install('git')
@@ -64,6 +91,7 @@ def install_git(force=False):
             for template in templates:
                 local('cat {0}.gitignore >> ~/.gitignore'.format(template))
 
+
 def install_zsh(force=False):
     _apt_get_install('zsh')
     local('chsh -s /usr/bin/zsh')
@@ -80,6 +108,7 @@ def install_zsh(force=False):
             local('rm -f ~/.{0}'.format(dotfile))
             local('ln -s $PWD/{0} ~/.{0}'.format(dotfile))
 
+
 def install_solarized_gnome_terminal(force=False, scheme='dark'):
     _apt_get_install('gnome-terminal', 'dconf-cli')
     git_repo = 'https://github.com/anthony25/gnome-terminal-colors-solarized'
@@ -87,6 +116,7 @@ def install_solarized_gnome_terminal(force=False, scheme='dark'):
     _git_pull_or_clone(git_repo, git_repo_dir)
     with lcd(git_repo_dir):
         local('./install.sh -s {0} -p Default'.format(scheme))
+
 
 def install_solarized_dircolors(force=False, scheme='dark'):
     _apt_get_install('coreutils')
@@ -97,6 +127,7 @@ def install_solarized_dircolors(force=False, scheme='dark'):
         with lcd(git_repo_dir):
             local('rm -f ~/.dir_colors')
             local('ln -s $PWD/dircolors.ansi-{0} ~/.dir_colors'.format(scheme))
+
 
 def install_solarized_gedit(force=False, scheme='dark'):
     _apt_get_install('gedit')
@@ -114,9 +145,11 @@ def _can_overwrite(file_or_dir):
     msg = 'Overwrite {0}?'.format(file_or_dir)
     return not os.path.exists(file_or_dir) or confirm(msg)
 
+
 def _apt_get_install(*packages):
     for package in packages:
-        local('sudo apt-get install {0}'.format(package))
+        local('sudo apt-get install --yes --force-yes {0}'.format(package))
+
 
 def _git_pull_or_clone(git_repo, git_repo_dir):
     if os.path.isdir(git_repo_dir):
