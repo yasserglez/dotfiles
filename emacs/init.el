b309df7ac2d5f6a9c1dbe9692aa99629ca7a1d14
Yasser Gonzalez <yasserglez@gmail.com>
2015-01-06 11:46:52 -0500

    Enable column-number-mode by default in Emacs

--- emacs/init.el
+++ emacs/init.el
@@ -3,5 +3,5 @@
 ;; Everything is contained in one Org-babel file that is loaded here.
 (require 'org)
 (setq vc-follow-symlinks nil)
-(defvar ygf/config-file "~/.emacs.d/config.org")
-(org-babel-load-file ygf/config-file)
+(defvar yasserglez/config-file "~/.emacs.d/config.org")
+(org-babel-load-file yasserglez/config-file)
